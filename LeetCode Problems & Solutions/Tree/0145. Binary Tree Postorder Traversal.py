b6ce0ce5dc11de3b99f995b31d5046b604467128
ali1299 <41929207+aliktl@users.noreply.github.com>
2020-08-21 19:33:07 +0900

    Update 0145. Binary Tree Postorder Traversal.py

--- LeetCode Problems & Solutions/Tree/0145. Binary Tree Postorder Traversal.py
+++ LeetCode Problems & Solutions/Tree/0145. Binary Tree Postorder Traversal.py
@@ -16,6 +16,11 @@ class Solution:
         response = dfs(root)
         return response
         
+  Explanation: 
+    1)Postorder meaning: left, right, root
+    2) So we just make recursion function for going to the all nodes
+    3) Make empty list and go through all nodes with postorder traversal and append the root.val 
+     
         
         
         
@@ -39,3 +44,23 @@ class Solution:
                 stack.append(node.left)
                 stack.append(node.right)
         return answer[::-1]
+    
+    
+    
+    Explanation:
+  1) This is iterative solution
+  2) Initially we check if there is a root or empty input
+  3) Create stack for using it in the while loop together with root inside
+  4) If stack is empty, we stop and return asnwer
+  5) While there is node in stack (stack is not empty), we pop the node from the stack
+  6) If the node is not empty, we append the value of this node to the answer
+  7) Lastly, we append the left and right nodes in the stack
+  8) The idea is to make reversed list , like root, right, left and reverse it in the end
+    
+    
+    
+    
+    
+    
+    
+    
