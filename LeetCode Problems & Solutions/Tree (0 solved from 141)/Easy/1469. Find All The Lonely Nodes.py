34bfbe95f654827d15d8d7407999aea654d655f6
ali1299 <41929207+aliktl@users.noreply.github.com>
2020-08-21 11:29:44 +0900

    Update 1469. Find All The Lonely Nodes.py

--- LeetCode Problems & Solutions/Tree (0 solved from 141)/Easy/1469. Find All The Lonely Nodes.py
+++ LeetCode Problems & Solutions/Tree (0 solved from 141)/Easy/1469. Find All The Lonely Nodes.py
@@ -1,4 +1,7 @@
 1469. Find All the Lonely Nodes
+https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+
 In a binary tree, a lonely node is a node that is the only child of its parent node. The root of the tree is not lonely because it does not have a parent node.
 
 Given the root of a binary tree, return an array containing the values of all lonely nodes in the tree. Return the list in any order.
