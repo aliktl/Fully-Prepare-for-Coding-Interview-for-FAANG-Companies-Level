1689977e615812becb2ebb4524ef2812f1a9c7e3
ali1299 <41929207+aliktl@users.noreply.github.com>
2020-08-21 19:33:48 +0900

    Update 0144. Binary Tree Preorder Traversal.py

--- LeetCode Problems & Solutions/Tree/0144. Binary Tree Preorder Traversal.py
+++ LeetCode Problems & Solutions/Tree/0144. Binary Tree Preorder Traversal.py
@@ -15,9 +15,12 @@ class Solution:
             return answer
         response = dfs(root)
         return response
-            
-            
-            
+             
+Explanation: 
+    1)Preorder meaning: root, left, right
+    2) So we just make recursion function for going to the all nodes
+    3) Make empty list and go through all nodes with preorder traversal and append the root.val 
+    
             
             
 # Definition for a binary tree node.
@@ -40,3 +43,19 @@ class Solution:
             root = stack.pop()
             root = root.right
         return answer
+    
+    
+Explanation:
+  1) This is iterative solution
+  2) Initially we check if there is a root or empty input
+  3) Create stack for using it in the while loop
+  4) If stack and root are empty, we stop and return asnwer
+  5) Firstly we appned the root.val to answer list. Next, while there is root.left (left root is not None), we append the root to stack
+  6) When there are no more root lefts we check for the root.right
+  7) The idea is to go to the left until you can, and when finished, go the the right, and again check for the all left roots
+    
+    
+    
+    
+    
+    
